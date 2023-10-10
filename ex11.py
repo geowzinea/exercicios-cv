243cd98989a39536a804702b2fb273085340a544
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-10-10 15:33:10 -0300

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,5 @@
+l = float(input('Digite a largura da parede: '))
+a = float(input('Digite a altura da parede: '))
+ar = l * a
+t = ar / 2
+print(f'Será necessário {t} litros de tinta para pintar uma área de {ar} metros ')
