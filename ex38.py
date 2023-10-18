0e39e6cf903f4922e4285dcb81cd96a8924e0d23
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-10-18 19:51:52 -0300

    Add files via upload

---
+++ ex38.py
@@ -0,0 +1,8 @@
+p1 = int(input('Digite um número: '))
+p2 = int(input('Digite outro número: '))
+if p1 > p2:
+    print('O PRIMEIRO número é maior')
+elif p2 >p1:
+    print('O SEGUNDO número é maior')
+elif p1 == p2:
+    print('AMBOS números são iguais')
