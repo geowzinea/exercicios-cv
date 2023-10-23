7a9648e9097c4e5087a5b0293324bf99328b1730
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-10-23 17:50:24 -0300

    Add files via upload

--- ex36.py
+++ ex36.py
@@ -4,8 +4,8 @@ anos = int(input('Em quantos anos você deseja pagar a casa? '))
 prestacao = casa / (anos * 12)
 minimo = salario * 30 / 100
 print(f'Para pagar uma casa de R${casa:.2f} em {anos} anos', end='')
-print(f'A prestação será de R${prestacao}')
-if salario > minimo:
+print(f'a prestação será de R${prestacao}')
+if salario <= minimo:
     print('\033[1;32mEmpréstimo CONCEDIDO!\033[m')
 else:
     print('\033[1;31mEmpréstimo NEGADO!\033[m')
