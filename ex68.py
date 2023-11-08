311f72d7f38c5179cadbe1c94f52e407efcf8e2c
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-11-07 21:29:46 -0300

    Add files via upload

---
+++ ex68.py
@@ -0,0 +1,9 @@
+from random import randint
+while True:
+    print('=~=' * 25)
+    print('JOGO ÍMPAR OU PAR')
+    print('=~=' * 25)
+    print('Escolha um número:')
+    print('Escolha ímpar ou par: ')
+    computador = randit
+    
