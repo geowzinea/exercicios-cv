866ccb3277ea2c79869123e47daf4cdf9df64022
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-12-07 22:13:05 -0300

    Add files via upload

---
+++ ex97.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f' {msg}')
+    print('~' * tam)
+    
+
+#Programa Principal    
+escreva('Geovanna Alves')
+escreva('Eu amo Luciano')
+escreva('Amo lu')
